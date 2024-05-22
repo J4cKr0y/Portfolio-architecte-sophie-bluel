b62ec6a76cfce359156759cb274c6097d778f50e
J4cKr0y <Kiky270281@gmail.com>
2024-05-23 00:21:19 +0200

    feat : formulaire de connexion fonctionnel

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,33 @@
+const login = "http://localhost:5678/api/users/login";
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const loginError = document.getElementById("login_error");
+const submitButton = document.getElementById("se_connecter");
+
+document.getElementById('se_connecter').addEventListener('click', async function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+
+    var data = {"email": email, "password": password};
+
+    try {
+        const response = await fetch(login, {
+            method: 'POST', 
+            headers: {'Content-Type': 'application/json',},
+            body: JSON.stringify(data),
+        });
+        if (response.status != 200) {
+            console.error('Erreur de connexion');
+            alert("ALARME ! Mauvais email ou mot de passe ! Déposez votre souris à terre et mettez les mains derrière la tête !");
+            return;
+        }
+        const logs = await response.json();
+        console.log('Succès:', logs);
+                
+            sessionStorage.setItem('connectOK', 'true'); // Stocke le fait que la connexion a été un succès    
+            window.location.href = "index.html"; // Redirige vers index.html en cas de succès
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+});
