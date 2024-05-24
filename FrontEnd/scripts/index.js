1ddf6a0cd3a8fb3dd85d2de2493ec82a759f2f71
J4cKr0y <Kiky270281@gmail.com>
2024-05-24 23:50:23 +0200

    fix : del onclick 4 event, filter color, mots login-out

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -10,26 +10,43 @@ function testifnull(truc) {
   if (typeof truc == 'null') {console.log(truc  +" est null");}
 }
 
-async function chargerMenu() {
+function isConnected() {
+  if (sessionStorage.getItem('connectOK') === 'true') {
+    const loginLink = document.querySelector('a[href="login.html"]');
+    if (loginLink) {
+      loginLink.href = "index.html";
+      loginLink.textContent = "logout";
+      loginLink.addEventListener('click', function() {sessionStorage.removeItem('connectOK');});
+    }
+  }
+}
+
+function boutonFiltreActif() {
+  document.querySelectorAll('#filtre > div').forEach(el => el.classList.remove('active')); // Supprime la classe 'active' partout
+  this.classList.add('active'); // Ajouter la classe 'active' à l'élément cliqué
+  appelerFonction(this.id); //envoi vers la fonction de tri
+}
+
+async function chargerFiltres() {
   // Effectuer la requête GET
   const response = await fetch(cat);
   const categories = await response.json();
   const divFiltre = document.getElementById('filtre');
-  // Parcourez les catégories et ajoutez-les au menu
   const tous = document.createElement('div');
+  tous.classList.add('active');
   tous.id = 'tous';
   tous.textContent = 'Tous';
-  tous.onclick = function() {appelerFonction(this.id);};
+  tous.addEventListener('click', boutonFiltreActif);
   divFiltre.appendChild(tous);
   categories.forEach(category => {
     const option = document.createElement('div');
     option.textContent = category.name;
-    option.id = category.name.split(' ')[0]; // La valeur de l'option (1er mot seulement)
-    option.onclick = function() {appelerFonction(this.id);};
+    option.id = category.name.split(' ')[0]; //(1er mot seulement)
+    option.addEventListener('click', boutonFiltreActif);
     divFiltre.appendChild(option);
   });
 }
-
+//<i class="fa-regular fa-pen-to-square"></i>
 let arts = []; //array
 async function chargerArticles() {
     try {
@@ -50,8 +67,8 @@ async function chargerArticles() {
       console.error('Il y a eu un problème avec l\'opération fetch: ' + erreur.message);
     }
 }
-function removeGallery() {
-  var conteneur = document.querySelector('.gallery');
+function resetGallery() {
+  const conteneur = document.querySelector('.gallery');
   while (conteneur.firstChild) {
     conteneur.removeChild(conteneur.firstChild);
   }
@@ -103,19 +120,19 @@ function fonction4() {
 function appelerFonction(valeur) {
   switch (valeur) {
     case 'tous':
-      removeGallery()
+      resetGallery()
       chargerArticles();
       break;
     case 'Objets':
-      removeGallery()
+      resetGallery()
       fonction2();
       break;
     case 'Appartements':
-      removeGallery()
+      resetGallery()
       fonction3();
       break;
     case 'Hotels':
-      removeGallery()
+      resetGallery()
       fonction4();
       break;
     default:
@@ -127,6 +144,7 @@ function appelerFonction(valeur) {
 
   // Appeler les fonctions au chargement de la page
   window.onload = function() {
-    chargerMenu();
+    isConnected();
+    chargerFiltres();
     chargerArticles();
   }
