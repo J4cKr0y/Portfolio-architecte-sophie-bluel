1cddb8b6e1ce54ae169727342f05f4afd23718a4
J4cKr0y <Kiky270281@gmail.com>
2024-06-18 22:59:01 +0200

    feat: adding routes for categories to able post, put and delete (sprint 3 BackEnd)

--- Backend/controllers/categories.controller.js
+++ Backend/controllers/categories.controller.js
@@ -18,3 +18,24 @@ exports.create = async (req, res) => {
 	})
 	return res.status(201).json(category)
 }
+
+exports.delete = async (req, res) => {
+	try{
+		await Categories.destroy({where:{id: req.params.id}})
+		return res.status(204).json({message: 'Category Deleted Successfully'})
+	}catch(e){
+		return res.status(500).json({error: new Error('Something went wrong')})
+	}
+}
+
+exports.update = async (req, res) => {
+	try{
+		const category = await Categories.update(
+			{name: req.body.name},
+			{where: {id: req.params.id}}
+		);
+		return res.status(200).json({message: 'Category Updated Successfully', category})
+	}catch(e){
+		return res.status(500).json({error: new Error('Something went wrong')})
+	}
+}
